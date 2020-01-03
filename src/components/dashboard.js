a2457f02bd8b64069079100c788ad4cf9b60bb8c
Justin Lampe <pierrelampre@gmail.com>
2020-01-03 14:05:29 -0500

    init committ

---
+++ src/components/dashboard.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class Dashboard extends Component {
+    constructor() {
+        super()
+    }
+
+
+    render() {
+    
+        return (
+            <div>
+                <h1>Welcome back!</h1>
+            </div>
+        )
+
+    }
+}
+
+export default Dashboard
